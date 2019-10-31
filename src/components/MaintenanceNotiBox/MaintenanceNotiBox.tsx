6aa047d0438f6574607e13b2b200ad395e2f82a3
min <0s.dongmin@gmail.com>
2019-10-31 21:58:27 +0900

    add window responsive style

--- src/components/MaintenanceNotiBox/MaintenanceNotiBox.tsx
+++ src/components/MaintenanceNotiBox/MaintenanceNotiBox.tsx
@@ -6,15 +6,17 @@ import styles from './MaintenanceNotiBox.scss'
 function MaintenanceNotiBox() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>
-        <Trans i18nKey="MaintenanceNotiBox.title">
-          <b />
-        </Trans>
-      </div>
-      <div className={styles.description}>
-        <Trans i18nKey="MaintenanceNotiBox.description">
-          <span className={styles.description__small} />
-        </Trans>
+      <div>
+        <div className={styles.title}>
+          <Trans i18nKey="MaintenanceNotiBox.title">
+            <b />
+          </Trans>
+        </div>
+        <div className={styles.description}>
+          <Trans i18nKey="MaintenanceNotiBox.description">
+            <span className={styles.description__small} />
+          </Trans>
+        </div>
       </div>
       <img className={styles.img} src={webDeveloperImg} alt="logo" />
     </div>
